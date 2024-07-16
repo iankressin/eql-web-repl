c8abf0ed1c6fb6906bdd13f05af50d7978d177e4
Ian Guimaraes <ianguimaraes31@gmail.com>
2024-07-16 17:09:42 -0300

    chore: update meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,7 +26,7 @@
 		content="EQL is a language that allows you to query the Ethereum Virtual Machine (EVM) using a SQL-like syntax. It is designed to be the simplest way to ask blockchain questions and get answers."
 	/>
 	<meta name="twitter:image" content="https://eql-app.vercel.app/twitter-card-image.png" />
-	<meta name="twitter:url" content="https://yourwebsite.com/page-url" />
+	<meta name="twitter:url" content="https://eql-app.vercel.app" />
 	<meta name="twitter:site" content="@iankguimaraes" />
 
 	<!-- General Meta Tags -->
@@ -40,6 +40,8 @@
 		content="ethereum, evm, ethereum query language, sql, eql, blockchain query language"
 	/>
 	<meta name="author" content="Ian K. Guimaraes" />
+	<meta property="og:url" content="https://eql-app.vercel.app" />
+	<meta property="og:logo" content="your value" />
 </svelte:head>
 
 <div class="bg-dim-1 text-white font-mono h-full">
